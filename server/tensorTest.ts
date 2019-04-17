a4cc76105e6dca40a65fe557889450c93d1beed2
Jørgen Tvedt <tvedtorama@gmail.com>
2019-04-17 07:25:59 -0400

    Running code sample.

---
+++ server/tensorTest.ts
@@ -0,0 +1,23 @@
+import * as tf from '@tensorflow/tfjs';
+import { Tensor, Rank } from '@tensorflow/tfjs';
+
+
+export const trainAModel = () => {
+	// Define a model for linear regression.
+	const model = tf.sequential();
+	model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+	// Prepare the model for training: Specify the loss and the optimizer.
+	model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+
+	// Generate some synthetic data for training.
+	const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+	const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+	// Train the model using the data.
+	model.fit(xs, ys).then(() => {
+		// Use the model to do inference on a data point the model hasn't seen before:
+		const tensor = ((x = model.predict(tf.tensor2d([5], [1, 1]))) => "length" in x ? null : x)()
+		tensor.print()
+	});
+}
