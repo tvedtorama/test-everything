e3e4075b2840d6bd72a5ff96e3ae59379145c85b
Jørgen Tvedt <tvedtorama@gmail.com>
2019-05-30 12:53:40 +0200

    Upgraded to using node version of tensorflow to avoid warnings in tests.

--- server/tensorTest.ts
+++ server/tensorTest.ts
@@ -1,5 +1,5 @@
-import * as tf from '@tensorflow/tfjs';
-import { Tensor, Rank } from '@tensorflow/tfjs';
+import * as tf from '@tensorflow/tfjs-node'
+// import * as tf from '@tensorflow/tfjs';
 
 
 export const trainAModel = () => {
